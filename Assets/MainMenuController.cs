0cdf8eef1849340849607d6ab23d7a2ff05bbb52
EvilHippy <64081107+EvilHippy@users.noreply.github.com>
2021-03-18 15:57:08 +0100

    Trying to make a menu
    Buttons are there, they're not working

---
+++ Assets/MainMenuController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+ 
+public class MainMenuController : MonoBehaviour
+{
+    public void multiplayer() {
+        SceneManager.LoadScene(5);
+    }
+ 
+    public void campaign() {
+        SceneManager.LoadScene(5);
+    }
+ 
+    public void exitGame() {
+        Application.Quit();
+    }
+}
