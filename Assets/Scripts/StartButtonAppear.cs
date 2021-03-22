8ee527b52652ef75062bc7c4a2fdb2ced3d2d2da
EvilHippy <64081107+EvilHippy@users.noreply.github.com>
2021-03-22 17:36:15 +0100

    MainScene and ChoiceScene with working buttons
    MainScene has 2 buttons, both work the same (going to ChoiceScene)
    ChoiceScene has 5 buttons in total, only 1 is visible at first.
    After pressing the tank button, it makes 3 more buttons appear (PositionButton1-3)
    After pressing any position button it makes Start button appear.
    After pressing start button it goes straight to the PlayScene

---
+++ Assets/Scripts/StartButtonAppear.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartButtonAppear : MonoBehaviour
+{
+
+    public GameObject Button;
+
+    public void OpenButton()
+    {
+        if(Button != null )
+        {
+            Button.SetActive(true);
+        }
+    }
+}
