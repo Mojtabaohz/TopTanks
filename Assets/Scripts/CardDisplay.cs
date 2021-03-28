e90649c76d350be6ec8acd2ef4ca5c34db5dc747
MojtabaOHz <moji.ostad@gmail.com>
2021-03-28 16:45:30 +0200

    Tanks cards are now scriptable objects

---
+++ Assets/Scripts/CardDisplay.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CardDisplay : MonoBehaviour
+{
+    public TankCard card;
+
+    public Text nameText;
+
+    public Text descriptionText;
+
+    public Image artworkImage;
+
+    public Text DamageText;
+
+    public Text healthText;
+
+    public Text armorText;
+    // Start is called before the first frame update
+    void Start()
+    {
+        nameText.text = card.name;
+        descriptionText.text = card.description;
+
+        artworkImage.sprite = card.artwork;
+        DamageText.text = card.damage.ToString();
+        healthText.text = card.health.ToString();
+        armorText.text = card.armor.ToString();
+    }
+    
+}
---
+++ Assets/Scripts/TankCard.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[CreateAssetMenu(fileName = "new TankCard", menuName = "TankCard")]
+public class TankCard : ScriptableObject
+{
+    public new string name;
+    public string description;
+    public Sprite artwork;
+
+    public int damage;
+    public int health;
+    public int armor;
+    
+    
+}
