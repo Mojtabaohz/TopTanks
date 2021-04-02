50c0b53e2ff83c9e3941032d53783e035b011215
EvilHippy <64081107+EvilHippy@users.noreply.github.com>
2021-04-02 15:13:30 +0200

    CardMaker is done
    VitalijCardCreating scene is created
    CardDisplayVitalij script is made
    Card script is made in folder Vitalij's Scripts
    ZTQ-15 prefab is made

---
+++ Assets/CardDisplayVitalij.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CardDisplayVitalij : MonoBehaviour
+{
+
+    public Card card;
+
+    public Text nameText;
+
+    public Image artworkImage;
+
+    public Text rankText;
+    public Text defenceText;
+    public Text powerText;
+    public Text mobilityText;
+    // Start is called before the first frame update
+    void Start()
+    {
+        nameText.text = card.name;
+
+        artworkImage.sprite = card.artwork;
+        
+        rankText.text = card.rank.ToString();
+        defenceText.text = card.defence.ToString();
+        powerText.text = card.power.ToString();
+        mobilityText.text = card.mobility.ToString();
+    }
+
+    
+}
---
+++ Assets/Scripts/Vitalij's Scripts/Card.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Card", menuName = "Card")]
+public class Card : ScriptableObject
+{
+   public new string name;
+
+   public Sprite artwork;
+
+   public int rank;
+   public int defence;
+   public int mobility;
+   public int power;
+
+   public void Print () {
+       Debug.Log(name + ": " + rank + " The Defence is: " + defence);
+   }
+}
