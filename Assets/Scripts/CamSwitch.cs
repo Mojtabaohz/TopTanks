0958be76db616eece14a67dc63eff72b9ddfbd3e
EvilHippy <64081107+EvilHippy@users.noreply.github.com>
2021-06-03 13:55:48 +0200

    Trying to do a CamSwitch script
    WIP
    Also changed the PlayScene

---
+++ Assets/Scripts/CamSwitch.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamSwitch : MonoBehaviour
+{
+    public GameObject cam1;
+    public GameObject cam2;
+ 
+  
+    void Update()
+    {
+        if (Input.GetButtonDown("Switch1"))
+        {
+            cam1.setActive(true);
+        
+            // cam1.getComponent<CameraTarget>
+            // cam2.setActive(false);
+        }
+        if (Input.GetButtonDown("Switch2"))
+        {
+            // cam1.setActive(false);
+            // cam2.setActive(true);
+        }
+    }
+}
