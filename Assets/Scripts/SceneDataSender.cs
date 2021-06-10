4cbfcd888083a459655d8c53fc6a9f206d9f67e5
MojtabaOHz <moji.ostad@gmail.com>
2021-06-10 15:57:25 +0200

    fix data tracker for play scene and result scene

---
+++ Assets/Scripts/SceneDataSender.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Analytics;
+using UnityEngine.SceneManagement;
+
+public class SceneDataSender : MonoBehaviour
+{
+    public float sceneTimer;
+    private String currentSceneName;
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentSceneName = SceneManager.GetActiveScene().name;
+    }
+
+    private void OnDisable()
+    {
+        AnalyticsResult timeSpent = Analytics.CustomEvent("Time Spent on Scene",
+            new Dictionary<string, object>
+            {
+                {currentSceneName, sceneTimer}
+            }
+        );
+        Debug.Log("Analytics Result"+ timeSpent +" : "+ sceneTimer );
+        Debug.Log("Analytics Result sceneName : "+ SceneManager.GetActiveScene().name );
+    }
+    // Update is called once per frame
+    public void Update()
+    {
+        sceneTimer += Time.deltaTime;
+        
+    }
+}
--- Assets/Scripts/SceneDataTracker.cs
+++ Assets/Scripts/SceneDataTracker.cs
@@ -7,47 +7,38 @@ using UnityEngine.SceneManagement;
 
 public class SceneDataTracker : MonoBehaviour
 {
-    public float sceneTimer;
-    private String currentScene;
+    
+    
+    private GameObject currentScene;
     private static bool created = false;
 
     void Awake()
     {
         //Debug.Log("Awake:" + SceneManager.GetActiveScene().name);
-
+    //TODO: Scene time tracker have problem with playscene. it will not track data unless it get activated 
         // Ensure the script is not deleted while loading.
         if (!created)
         {
             DontDestroyOnLoad(this.gameObject);
             created = true;
         }
-        else
-        {
-            //Destroy(this.gameObject);
-        }
-    }
-
-    public void Start()
-    {
-        currentScene = SceneManager.GetActiveScene().name;
-    }
-
-    public void Update()
-    {
-        sceneTimer += Time.deltaTime;
         
     }
 
+    
+
+    public void Start()
+    {
+        //currentSceneName = SceneManager.GetActiveScene().name;
+        currentScene = new GameObject("currentSceneHolder");
+        currentScene.AddComponent<SceneDataSender>();
+    }
+
+    
+
     public void LoadScene(string sceneName)
     {
-        AnalyticsResult timeSpent = Analytics.CustomEvent("Time Spent on Scene" ,
-            new Dictionary<string, object>
-        {
-            {currentScene , sceneTimer} 
-        } 
-            );
-        //Debug.Log("Analytics Result"+ timeSpent +" : "+ sceneTimer );
-        //Debug.Log("Analytics Result sceneName : "+ currentScene );
+        
         SceneManager.LoadScene(sceneName);
     }
 
